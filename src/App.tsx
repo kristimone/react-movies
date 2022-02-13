bfe3b4449997dff31ba28c78f48fe36184ac2539
Kristi Mone <kristi.mone@ikubinfo.al>
2022-02-13 14:17:20 +0100

    React commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import routes from './route-config'
+import configureValidations from './Validations';
+import {claim} from './auth/auth.models';
+import { useEffect, useState } from 'react';
+import AuthenticationContext from './auth/AuthenticationContext';
+import { getClaims } from './auth/handleJWT';
+import configureInterceptor from './utils/httpInterceptors';
+
+configureValidations();
+configureInterceptor();
 
 function App() {
+  
+  const [claims, setClaims] = useState<claim[]>([]);
+
+  useEffect(() => {
+    setClaims(getClaims())
+  }, [])
+
+  function isAdmin(){
+    return claims.findIndex(claim => claim.name === 'role' && claim.value === 'admin') > -1;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <AuthenticationContext.Provider value={{claims, update: setClaims}}>
+      <Menu />
+      <div className="container">
+        <Switch>
+          {routes.map(route => 
+          <Route key={route.path} path={route.path} exact={route.exact}>
+            {route.isAdmin && !isAdmin() ? <>
+            You are not allowed to see this page
+            </>: <route.component />}           
+          </Route>)}
+        </Switch>
+      </div>
+      <footer className="bd-footer py-5 mt-5 bg-light">
+            <div className="container">
+                React Movies {new Date().getFullYear().toString()}
+            </div>
+      </footer>
+      </AuthenticationContext.Provider>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css'; // global
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStar);
 
 ReactDOM.render(
   <React.StrictMode>
